b98b36c0cbb2acce5ec54b927cb335af007b21b0
Krishnageeth <krishnageeth11@gmail.com>
2023-06-19 15:22:22 +0530

    Added 2 more test methods

--- src/test/java/com/automation/testng/TitleTest.java
+++ src/test/java/com/automation/testng/TitleTest.java
@@ -10,13 +10,35 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TitleTest {
 	@Test
-	public void verifyTitle() {
+	public void verifyGoogleTitle() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver webDriver = new ChromeDriver();
 		webDriver.manage().window().maximize();
 		webDriver.get("https://www.google.com");
 		String title = webDriver.getTitle();
 		assertEquals("Google", title);
+		webDriver.close();
+	}
+
+	@Test
+	public void verifyFacebokTitle() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver webDriver = new ChromeDriver();
+		webDriver.manage().window().maximize();
+		webDriver.get("https://www.facebook.com");
+		String title = webDriver.getTitle();
+		assertEquals("Facebook – log in or sign up", title);
+		webDriver.close();
+	}
+
+	@Test
+	public void verifyAlphabet() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver webDriver = new ChromeDriver();
+		webDriver.manage().window().maximize();
+		webDriver.get("https://abc.xyz/");
+		String title = webDriver.getTitle();
+		assertEquals("Alphabet", title);
 		webDriver.close();
 	}
 }
